0188f7bd1fdb513360424edc332d3e0df988cf70
Csbw0319 <cwilkins0319@icloud.com>
2022-07-19 12:24:02 -0400

    commit

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import Popular from "../components/Popular";
+import Random from "../components/IngredientSearch";
+
+import React from 'react'
+import IngredientSearch from "../components/IngredientSearch";
+
+function Home() {
+  return (
+    <div>
+    <Popular />
+    </div>
+  )
+}
+
+export default Home
